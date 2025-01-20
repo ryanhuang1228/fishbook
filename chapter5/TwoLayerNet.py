fe956a0c2cf97439a7b88c95c65fb535e24ff833
ryanhuang1228 <ryanhuang1228@outlook.com>
2025-01-20 15:20:38 +0800

    finish chapter5 and introduce layers.py into common directory from original code.

---
+++ chapter5/TwoLayerNet.py
@@ -0,0 +1,126 @@
+import numpy as np
+from collections import OrderedDict
+from common.functions import softmax, cross_entropy_error
+from common.gradient import numerical_gradient
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+    
+    def forward(self, x):
+        self.x = x
+        y = np.dot(x, self.W) + self.b
+        return y
+    
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)  # 以axis=0为轴，沿列方向相加
+        return dx
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+    
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+    
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.t = None
+        self.y = None
+        self.loss = None
+    
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        
+        return self.loss
+    
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]            # 该方法仅适用于批处理
+                                                # 除以batch_size的意思是为了使得对权重w的更新归一化
+        if self.y.size == self.t.size:
+            dx = (self.y - self.t) / batch_size # one-hot-vector处理
+        else:
+            dx = self.y.copy()
+            dx[np.arange(batch_size), self.t] -= 1  # 对标签t下的导数减去1
+            dx = dx / batch_size                
+        return dx
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        self.params = {}
+        self.params["W1"] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params["b1"] = np.zeros(hidden_size)
+        self.params["W2"] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params["b2"] = np.zeros(output_size)
+
+        # 生成层
+        self.layers = OrderedDict()
+        self.layers["Affine1"] = Affine(self.params["W1"], self.params["b1"])
+        self.layers["Relu1"] = Relu()
+        self.layers["Affine2"] = Affine(self.params["W2"], self.params["b2"])
+        self.lastLayer = SoftmaxWithLoss()
+
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x
+    
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.lastLayer.forward(y, t)
+    
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        if t.ndim != 1:
+            t = np.argmax(t, axis=1)
+        accuracy = np.sum(y==t) / float(x.shape[0])
+        return accuracy
+    
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+        
+        grads = {}
+        grads["W1"] = numerical_gradient(loss_W, self.params["W1"])
+        grads["b1"] = numerical_gradient(loss_W, self.params["b1"])
+        grads["W2"] = numerical_gradient(loss_W, self.params["W2"])
+        grads["b2"] = numerical_gradient(loss_W, self.params["b2"])
+
+        return grads
+    
+    def gradient(self, x, t):
+        # forward
+        self.loss(x, t)
+
+        # backward
+        dout = 1
+        dout = self.lastLayer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+        
+        grads = {}
+        grads["W1"] = self.layers["Affine1"].dW
+        grads["b1"] = self.layers["Affine1"].db
+        grads["W2"] = self.layers["Affine2"].dW
+        grads["b2"] = self.layers["Affine2"].db
+        return grads
+    
--- common/functions.py
+++ common/functions.py
@@ -51,7 +51,7 @@ def cross_entropy_error(y, t):
     # 监督数据是one-hot-vector的情况下，转换为正确解标签的索引
     if t.size == y.size:
         t = t.argmax(axis=1)
-             
+           
     batch_size = y.shape[0]
     return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
